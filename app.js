600ee562ee2734359b6ace4739b80d518af3c934
SayantanBar <121619214+SayantanBar@users.noreply.github.com>
2026-05-12 20:45:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App is running!");
+  res.send("App is running on aws!");
 });
 
 app.listen(8000, "0.0.0.0", () => {
